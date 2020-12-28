d54a7281a3a5aa7af35064421af362f05a226dc6
Rob <roberto.reale.ja@gmail.com>
2020-12-28 12:52:34 +0900

    Set up eslint for front end

--- customer/src/App.tsx
+++ customer/src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-export default function App() {
+export default function App(): JSX.Element {
   const [users, setUsers] = useState(null)
   useEffect(() => {
     const getData = async () => {
