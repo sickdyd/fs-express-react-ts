1bb6d4121f5118fd51d3cfa0805ae3ebe69da06e
Rob <roberto.reale.ja@gmail.com>
2020-12-28 14:47:40 +0900

    Add basic testing for API

--- api/src/routes/users.ts
+++ api/src/routes/users.ts
@@ -2,7 +2,7 @@ import { Router } from 'express'
 const users = Router()
 
 users.get('/', function (req, res) {
-  res.send('users list')
+  res.status(200).send('users list')
 })
 
 export default users
---
+++ api/tests/routes/users.test.ts
@@ -0,0 +1,11 @@
+const request = require('supertest')
+import app from '@src/app'
+
+describe('/users', () => {
+  it('should return the list of users', async () => {
+    const res = await request(app).get('/users')
+
+    expect(res.status).toEqual(200)
+    expect(res.text).toEqual('users list')
+  })
+})
