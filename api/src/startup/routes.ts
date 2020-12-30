58bd590525084b2b1e2ca429365b2d121286f2f5
Rob <roberto.reale.ja@gmail.com>
2020-12-30 22:50:10 +0900

    Fix lint and typecheck errors

--- api/src/startup/routes.ts
+++ api/src/startup/routes.ts
@@ -2,7 +2,7 @@ import { Express } from 'express'
 import indexRouter from '../routes/index'
 import usersRouter from '../routes/users'
 
-export default function routes(app: Express): number {
+export default function routes(app: Express): void {
   app.use('/', indexRouter)
   app.use('/users', usersRouter)
 }
